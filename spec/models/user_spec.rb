669691813baa1928c0f9c46dab7015d747480a7f
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-28 12:02:21 +0100

    add pending tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without an email"
+  it "is invalid without a name"
+  it "is invalid without password"
 end
