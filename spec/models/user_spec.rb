5cdd3d2787698855d62fae5e433d4a94a29bfeab
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-28 12:37:18 +0100

    refactor the user spec according to guidelines

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 require 'factory_girl_rails'
 
 RSpec.describe User, type: :model do
-  before :each do
-    @user = FactoryGirl.create(:user)
-  end
+  let(:user) { FactoryGirl.create :user }
+
   it "is invalid without an email" do
-    @user.should be_valid
+    expect(user).to be_valid
   end
   it "is invalid without a name" do
-    @user.name = nil
-    @user.should_not be_valid
+    user.name = nil
+    expect(user).to_not be_valid
   end
   it "is invalid without password" do
-    @user.password = nil
-    @user.should_not be_valid
+    user.password = nil
+    expect(user).to_not be_valid
   end
 end
